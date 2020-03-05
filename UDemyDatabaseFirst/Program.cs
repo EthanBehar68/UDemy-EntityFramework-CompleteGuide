713a48192a319dbef4c35a4ba012b71bee59e773
Ethan Behar <behar.ethan@gmail.com>
2020-03-05 18:49:09 -0500

    Section 3 Exercises 1,2,3

--- UDemyDatabaseFirst/Program.cs
+++ UDemyDatabaseFirst/Program.cs
@@ -14,6 +14,13 @@ namespace UDemyDatabaseFirst
         Advanced = 3
     }
 
+    public enum Classification : byte
+    {
+        Silver = 1,
+        Gold = 2,
+        Platinum = 3
+    }
+
     class Program
     {
         static void Main(string[] args)
@@ -32,10 +39,14 @@ namespace UDemyDatabaseFirst
 
             //var course = new Course();
             //course.Level = Level.Beginner; // 1
-
-            // Section 3 Exercise '1st Iteration'
-            // See '_Extras' for documents regarding this Exercise
-            //var context = new VidzyDbContext();
+            
+            
+            ///
+            /// Section 3 Exercise '1st Iteration'
+            /// See '_Extras' for documents regarding this Exercise
+            /// 
+            
+            var context = new VidzyDbContext();
 
             //var comedyGenre = context.Genres.SingleOrDefault(g => g.Name == "Comedy");
             //var actionGenre = context.Genres.SingleOrDefault(g => g.Name == "Action");
@@ -45,11 +56,33 @@ namespace UDemyDatabaseFirst
             //context.AddVideo("Matrix", new DateTime(1999, 3, 31), actionGenre.Name);
             //context.AddVideo("Deadpool 2", new DateTime(2018, 5, 10), comedyGenre.Name);
 
-            //var familyGenre = context.Genres.SingleOrDefault(g => g.Name == "Family");
+            // Be careful of uncommenting multiple SaveChanges() calls
+            //context.SaveChanges();
+
+            ///
+            /// Section 3 Exercise '2nd Iteration'
+            /// See '_Extras' for documents regarding this Exercise
+            /// 
+
+            var familyGenre = context.Genres.SingleOrDefault(g => g.Name == "Family");
 
             //context.AddVideo("Toy Story", new DateTime(1995, 11, 22), familyGenre.Name);
 
             //context.SaveChanges();
+
+            ///
+            /// Section 3 Exercise '3rd Iteration'
+            /// See '_Extras' for documents regarding this Exercise
+            /// 
+
+            var romanticGenre = context.Genres.SingleOrDefault(g => g.Name == "Romance");
+
+            context.AddVideo("Titanic", new DateTime(1995, 11, 22), romanticGenre.Name, Classification.Platinum);
+            context.AddVideo("Toy Story 2", new DateTime(1999, 11, 13), familyGenre.Name, Classification.Gold);
+            context.AddVideo("Snow White", new DateTime(1952, 2, 13), romanticGenre.Name, Classification.Silver);
+
+            context.SaveChanges();
+
         }
     }
 }
