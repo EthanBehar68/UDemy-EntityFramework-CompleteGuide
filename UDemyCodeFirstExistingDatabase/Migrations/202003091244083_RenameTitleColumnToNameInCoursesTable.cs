1859ada6e29c6cbdabc096cb4d9716d10137391c
Ethan Behar <behar.ethan@gmail.com>
2020-03-09 08:50:28 -0400

    Code First - Modifying existing classes - adding new columns, changing names of columns, deleting existing columns

---
+++ UDemyCodeFirstExistingDatabase/Migrations/202003091244083_RenameTitleColumnToNameInCoursesTable.cs
@@ -0,0 +1,24 @@
+namespace UDemyCodeFirstExistingDatabase.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class RenameTitleColumnToNameInCoursesTable : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Courses", "Name", c => c.String(nullable: false));
+            Sql("UPDATE Courses SET Name = Title");
+            DropColumn("dbo.Courses", "Title");
+            //RenameColumn("dbo.Courses", "Title", "Name");
+
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Courses", "Title", c => c.String(nullable: false));
+            Sql("UPDATE Courses SET Title = Name");
+            DropColumn("dbo.Courses", "Name");
+        }
+    }
+}
