cab223f4863de4d601edef6eeccca08db0fd36a2
Ethan Behar <behar.ethan@gmail.com>
2020-03-09 17:08:23 -0400

    Section 5 Exercises - 4th iteration complete

--- UDemyCodeFirstVidzy/Video.cs
+++ UDemyCodeFirstVidzy/Video.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace UDemyCodeFirstVidzy
 {
-    public enum Classification
+    public enum Classification : byte
     {
         Silver = 1,
         Gold = 2,
@@ -13,9 +13,14 @@ namespace UDemyCodeFirstVidzy
     public class Video
     {
         public int Id { get; set; }
+        
         public string Name { get; set; }
+        
         public DateTime ReleaseDate { get; set; }
+        
         public Genre Genre { get; set; }
+        public byte GenreId { get; set; }
+
         public Classification Classification { get; set; }
     }
 }
--- UDemyCodeFirstVidzy/VidzyDbContext.cs
+++ UDemyCodeFirstVidzy/VidzyDbContext.cs
@@ -17,5 +17,25 @@ namespace UDemyCodeFirstVidzy
 
         public DbSet<Video> Videos { get; set; }
         public DbSet<Genre> Genres { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Video>()
+                .Property(v => v.Name)
+                .IsRequired()
+                .HasMaxLength(255);
+
+            modelBuilder.Entity<Video>()
+                .HasRequired(v => v.Genre)
+                .WithMany(g => g.Videos)
+                .HasForeignKey(v => v.GenreId)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Genre>()
+                .Property(g => g.Name)
+                .IsRequired()
+                .HasMaxLength(255);
+
+        }
     }
 }
