434b2c6d3245627924259c8e9c47dc400af18366
Ethan Behar <behar.ethan@gmail.com>
2020-03-09 16:15:18 -0400

    code first - fluent api code organization best practices

---
+++ DemoFluentAPI/EntitiyConfigurations/CourseConfiguration.cs
@@ -0,0 +1,44 @@
+﻿using System.Data.Entity.ModelConfiguration;
+
+namespace FluentAPI
+{
+    public class CourseConfiguration : EntityTypeConfiguration<Course>
+    {
+        public CourseConfiguration()
+        {
+            // Convention for organization (Alphabetical)
+
+            // Table overrides at beginning (Alphabetical)
+
+            // Primary Key overrides (Alphabetical)
+
+            // Property Configurations (Alphabetical)
+            Property(c => c.Description)
+            .IsRequired()
+            .HasMaxLength(2000);
+
+            Property(c => c.Name)
+            .IsRequired()
+            .HasMaxLength(255);
+
+            // Relationship Configurations (Alphabetical)
+
+            HasRequired(c => c.Author)
+            .WithMany(a => a.Courses)
+            .HasForeignKey(c => c.AuthorId)
+            .WillCascadeOnDelete(false);
+
+            HasRequired(c => c.Cover)
+            .WithRequiredPrincipal(c => c.Course);
+
+            HasMany(c => c.Tags)
+            .WithMany(t => t.Courses)
+            .Map(m =>
+                {
+                    m.ToTable("CourseTags");
+                    m.MapLeftKey("CourseId");
+                    m.MapRightKey("TagId");
+                });
+        }
+    }
+}
