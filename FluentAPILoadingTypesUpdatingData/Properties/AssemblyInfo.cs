c20aa9450a7478761bfe4f6c8d30deb8f3809ac7
Ethan Behar <behar.ethan@gmail.com>
2020-03-10 12:16:59 -0400

    updating all assembly attributes to match namespace

--- FluentAPILoadingTypesUpdatingData/Properties/AssemblyInfo.cs
+++ FluentAPILoadingTypesUpdatingData/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DataAnnotations")]
+[assembly: AssemblyTitle("FluentAPILoadingTypesUpdatingData")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DataAnnotations")]
+[assembly: AssemblyProduct("FluentAPILoadingTypesUpdatingData")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- VidzyQueriesExercise/Properties/AssemblyInfo.cs
+++ VidzyQueriesExercise/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Vidzy")]
+[assembly: AssemblyTitle("VidzyQueriesExercise")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Vidzy")]
+[assembly: AssemblyProduct("VidzyQueriesExercise")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
